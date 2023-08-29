12c94b206030765115c5f9fb9f335b0d6e8fa0e7
Hoyoung Lee <sohostory@gmail.com>
2023-08-29 08:18:24 -0500

    functional exercises completed

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+from functools import reduce
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#1 Capitalize all of the pet names and print the list
+my_pets = ['sisi', 'bibi', 'titi', 'carla']
 
+def capitalize_word(word):
+    return word.upper()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print(list(map(capitalize_word, my_pets)))
 
+#2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
+my_strings = ['a', 'b', 'c', 'd', 'e']
+my_numbers = [5,4,3,2,1]
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(list(zip(my_strings, sorted(my_numbers))))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#3 Filter the scores that pass over 50%
+scores = [73, 20, 65, 19, 76, 100, 88]
+
+def passing_scores(item):
+    return item >= 50
+
+print(list(filter(passing_scores, scores)))
+
+#4 Combine all of the numbers that are in a list on this file using reduce
+# (my_numbers and scores). What is the total?
+
+def accumulator(acc, item):
+    return acc + item
+
+print(int(reduce(accumulator, (my_numbers + scores), 0)))
